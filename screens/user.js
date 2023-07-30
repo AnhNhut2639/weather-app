5f03e2f1cd5ddf9040de655177dc75c0e020c774
AnhNhut2639 <zoro92211@gmail.com>
2023-07-30 21:47:36 +0700

    feat: added tailwindcss into project

---
+++ screens/user.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function User() {
+  return (
+    <View>
+      <Text className="text-green-500 text-3xl">Users</Text>
+    </View>
+  );
+}
